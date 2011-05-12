50a7467c2439a265d40547dfed704baedb7652cb
Krzysztof Plocharz <qreesp@gmail.com>
2011-05-13 00:14:46 +0200

    use stl library

--- jni/Android.mk
+++ jni/Android.mk
@@ -6,8 +6,7 @@ LOCAL_MODULE    := libgl2jni
 LOCAL_CFLAGS    := -Werror
 LOCAL_SRC_FILES := gl_code.cpp
 LOCAL_LDLIBS    := -llog -lGLESv2 -lstdc++
-APP_STL := stlport_static
-LOCAL_STATIC_LIBRARIES := stlport_static
+LOCAL_SHARED_LIBRARIES := stlport_static
 LOCAL_C_INCLUDES := /home/qrees/bin/android-ndk-r5b/sources/cxx-stl/stlport/stlport
 
 include $(BUILD_SHARED_LIBRARY)
